2d349ee4deedcb6efc6ddbd6e20477a024ad7bf6
Chigozie Madubuko <chigoziemadubuko@gmail.com>
2022-07-24 18:08:27 +0100

    Promote to production

--- main.sh
+++ main.sh
@@ -1,10 +1,9 @@
-#!/bin/bash
-# export BACKEND_IP=$(aws ec2 describe-instances --query 'Reservations[*].Instances[*].PublicIpAddress' --filters "Name=tag:project,Values=udapeople" --output text)
-export API_URL="http://3.83.43.224:3030"
-echo "API_URL = ${API_URL}"
+# export OldWorkflowID=$(aws cloudformation \
+#   list-exports --query "Exports[?Name==\`WorkflowID\`].Value" \
+#   --no-paginate --output text)
+# echo $OldWorkflowID >>./OldWorkflowID.txt
+# cat ~/OldWorkflowID.txt
 
-if curl "${API_URL}/api/status" | grep "ok"; then
-  return 0
-else
-  return 1
-fi
+# WorkflowID=$(cat ./OldWorkflowID.txt)
+# # echo OldWorkflowID: "${OldWorkflowID}"
+# echo WorkflowID: "${WorkflowID}"
