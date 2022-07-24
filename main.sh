244be22d3c4e2b5cf4e06ccedc3d0609d64b1208
Chigozie Madubuko <chigoziemadubuko@gmail.com>
2022-07-24 13:36:49 +0100

    Smoke test6

--- main.sh
+++ main.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-export BACKEND_IP=$(aws ec2 describe-instances --query 'Reservations[*].Instances[*].PublicIpAddress' --filters "Name=tag:project,Values=udapeople" --output text)
-export API_URL="http://${BACKEND_IP}:3030"
+# export BACKEND_IP=$(aws ec2 describe-instances --query 'Reservations[*].Instances[*].PublicIpAddress' --filters "Name=tag:project,Values=udapeople" --output text)
+export API_URL="http://3.83.43.224:3030"
 echo "API_URL = ${API_URL}"
+
+if curl "${API_URL}/api/status" | grep "ok"; then
+  return 0
+else
+  return 1
+fi
