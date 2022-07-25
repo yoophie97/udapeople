a4fbf2616c8aedd2af54a1c7d56dde4bc3f09dc3
Chigozie Madubuko <chigoziemadubuko@gmail.com>
2022-07-25 09:34:30 +0100

    cleanup

--- main.sh
+++ main.sh
@@ -1,9 +1,19 @@
-# export OldWorkflowID=$(aws cloudformation \
-#   list-exports --query "Exports[?Name==\`WorkflowID\`].Value" \
-#   --no-paginate --output text)
-# echo $OldWorkflowID >>./OldWorkflowID.txt
-# cat ~/OldWorkflowID.txt
+# Fetch the Old workflow ID
+export OldWorkflowID=$(aws cloudformation \
+  list-exports --query "Exports[?Name==\`ID\`].Value" \
+  --no-paginate --output text)
+echo OldWorkflowID: "${OldWorkflowID}"
 
-# WorkflowID=$(cat ./OldWorkflowID.txt)
-# # echo OldWorkflowID: "${OldWorkflowID}"
-# echo WorkflowID: "${WorkflowID}"
+# Fetch the stack names
+export STACKS=($(aws cloudformation list-stacks --query "StackSummaries[*].StackName" \
+  --stack-status-filter CREATE_COMPLETE --no-paginate --output text))
+echo Stack names: "${STACKS[@]}"
+
+export OldWorkflowID="${OldWorkflowID}"
+export STACKS=[$STACKS]
+
+if [[ "${CIRCLE_WORKFLOW_ID:0:7}" != "${OldWorkflowID}" ]]; then
+  aws s3 rm "s3://udapeople-${OldWorkflowID}" --recursive
+  aws cloudformation delete-stack --stack-name "udapeople-backend-${OldWorkflowID}"
+  aws cloudformation delete-stack --stack-name "udapeople-frontend-${OldWorkflowID}"
+fi
