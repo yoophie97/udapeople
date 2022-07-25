fd04c007ef18737a2fa9ef1d4dffead3ff853356
Chigozie Madubuko <chigoziemadubuko@gmail.com>
2022-07-25 17:38:06 +0100

    configure infra

--- main.sh
+++ main.sh
@@ -1,3 +1,6 @@
-export STACKS=($(aws cloudformation list-stacks --query "StackSummaries[*].StackName" \
-        --stack-status-filter CREATE_COMPLETE --no-paginate --output text)) 
-echo Stack names: "${STACKS[@]}"        
+#!/bin/sh -eo pipefail
+export STACKS=$(aws cloudformation list-stacks --query "StackSummaries[*].StackName" \
+  --stack-status-filter CREATE_COMPLETE --no-paginate --output text)
+
+echo Stack names: "${STACKS[@]}"
+export STACKS=[$STACKS]
