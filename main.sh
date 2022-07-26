891fb38b4291a7ce9c9a5af132519931ddc049f6
Chigozie Madubuko <chigoziemadubuko@gmail.com>
2022-07-26 16:47:53 +0100

    Project submission

--- main.sh
+++ main.sh
@@ -1,3 +1,5 @@
-export BACKEND_IP=$(aws ec2 describe-instances --query 'Reservations[*].Instances[*].PublicIpAddress' --filters "Name=tag:Name,Values='backend-5b40e82'" --output text)
-export API_URL=http://${BACKEND_IP}:3030
-echo API_URL=${API_URL} >> frontend/.env
+# export BACKEND_IP=$(aws ec2 describe-instances --query 'Reservations[*].Instances[*].PublicIpAddress' --filters "Name=tag:Name,Values='backend-5b40e82'" --output text)
+# export API_URL=http://${BACKEND_IP}:3030
+# echo API_URL=${API_URL} >> frontend/.env
+
+aws cloudfront create-invalidation  --distribution-id E1CW4T6VASH1LT --paths "/*"
