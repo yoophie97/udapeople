8864111ce7d0dae0e272d60814cfeeb6d1ece81d
Chigozie Madubuko <chigoziemadubuko@gmail.com>
2022-07-26 14:19:45 +0100

    Testing

--- main.sh
+++ main.sh
@@ -1,3 +1,3 @@
-export BACKEND_IP=$(aws ec2 describe-instances --query 'Reservations[*].Instances[*].PublicIpAddress' --filters "Name=tag:project,Values=udapeople" --output text)
+export BACKEND_IP=$(aws ec2 describe-instances --query 'Reservations[*].Instances[*].PublicIpAddress' --filters "Name=tag:Name,Values='backend-5b40e82'" --output text)
 export API_URL=http://${BACKEND_IP}:3030
 echo API_URL=${API_URL} >> frontend/.env
